2a3fbb74b78f8c8819b02ebc439a1dcd8007a755
Brandon Flannery <brandonjflannery@berkeley.edu>
2016-02-23 16:59:42 -0800

    New BloombergSearch class

--- news_scraping.py
+++ news_scraping.py
@@ -28,52 +28,113 @@ import urllib2
 
 # ## Get A BeautifulSoup Object
 
-# In[11]:
+# In[110]:
 
-def get_search_soup(text):
-    url =  ('http://www.bloomberg.com/search?query=' + str(text))
-    soup = get_soup(url)
-    return soup
-def get_soup(url):
-    page = urllib2.urlopen(url).read()
-    soup = BeautifulSoup(page)
-    return soup
-def get_search_page_links(soup):
-    article_list = []
-    for item in soup.find_all('h1'):
+class BloombergSearch:
+    def __init__(self, search_term):
+        self.search_term = search_term
+        self.url_page1 = ('http://www.bloomberg.com/search?query=' + str(self.search_term))
+        self.curr_soup = BeautifulSoup(urllib2.urlopen(self.url_page1).read())
+    
+    
+    def get_search_soup(self):
+        url =  self.url_page1
+        soup = self.get_soup(url)
+        return soup
+    def get_soup(self, url):
+        page = urllib2.urlopen(url).read()
+        soup = BeautifulSoup(page)
+        return soup
+    def get_search_page_links(self, num_pages):
+        article_list = []
+        for i in range(1, num_pages + 1):
+            temp_soup = self.get_soup(self.url_page1 + str('&page=') + str(i))
+            for result in temp_soup.find_all('h1'):
+                try:
+                    if 'video' in result.a['href']:
+                        continue
+                    if 'http' in result.a['href']:
+                        #print item.a['href']
+                        article_list.append(result.a['href'])
+                    else:
+                        #print 'http://www.bloomberg.com/' + item.a['href']
+                        article_list.append('http://www.bloomberg.com/' + result.a['href'])
+                except:
+                    continue
+            #print 'Added page=' + str(i)
+        return article_list
+    
+    def get_post_body(self, article_soup = self.curr_soup):
+        final_text = ""
+        self.curr_soup = article_soup
+        query = article_soup.find_all('div',  class_="article-body__content")
+        for item in query:
+            for text in item.find_all('p'):
+                final_text = final_text + '\n\n' + str(text.text.encode('utf-8'))
+        if final_text == "":
+            return 0
+        return final_text
+    
+    def get_post_date(self, article_soup = self.curr_soup):
+        final_text = ""
+        self.curr_soup = article_soup
+        result = article_soup.find('time', class_ = "published-at")
         try:
-            if 'video' in item.a['href']:
-                #print 'Video Link Skipped: ' + item.a['href']
-                continue
-            if 'http' in item.a['href']:
-                #print item.a['href']
-                article_list.append(item.a['href'])
-            else:
-                #print 'http://www.bloomberg.com/' + item.a['href']
-                article_list.append('http://www.bloomberg.com/' + item.a['href'])
+            return result['datetime']
         except:
-            continue
-    return article_list
-def get_text_body(article_url):
-    final_text = ""
-    soup = get_soup(article_url)
-    query = soup.find_all('div',  class_="article-body__content")
-    for item in query:
-        for text in item.find_all('p'):
-            final_text = final_text + '\n\n' + str(text.text.encode('utf-8'))
-    return final_text
+            return None
+    
+    def get_post_author(self, article_soup = self.curr_soup):
+        final_text = ""
+        self.curr_soup = article_soup
+        result = article_soup.find('a', class_ = "author-link")
+        try:
+            return result.text.lstrip().rstrip()
+        except:
+            return None
+    
+    def get_post_title(self, article_soup = scurr_soup):
+        final_text = ""
+        self.curr_soup = article_soup
+        result = article_soup.find('title')
+        try:
+            return result.text.lstrip().rstrip()
+        except:
+            return None
+    
+    def make_info(self, pages = 1):
+        final_df = pd.DataFrame()
+        for url in self.get_search_page_links(pages):
+            body = self.get_post_body(url)
+            title = self.get_post_body()
+            author = self.get_post_author()
+            date = self.get_post_date()
+            temp_series = pd.Series([title, author, date, body])
+            final_df= final_df.append(temp_series, ignore_index = True)
+        final_df.columns = ['title', 'author', 'date', 'text']
+        return final_df
 
 
-# In[12]:
-
-soup = get_search_soup('apple')
+# In[ ]:
 
 
-# In[13]:
 
-url_list = get_search_page_links(soup)
-temp = get_text_body(url_list[1])
-print temp
+
+# In[100]:
+
+bloom_obj = BloombergSearch('apple')
+
+
+# In[102]:
+
+temp = bloom_obj.make_info(3)
+print len(url_list)
+temp
+
+
+# In[ ]:
+
+
 
 
 # In[ ]:
